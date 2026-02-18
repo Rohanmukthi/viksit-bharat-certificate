08603791b9b506b0f664cbf2a4af25ddfcecc5eb
Rohanmukthi <149431725+Rohanmukthi@users.noreply.github.com>
2026-02-18 15:59:39 +0530

    Initial certificate system

---
+++ script.js
@@ -0,0 +1,17 @@
+function generateCertificate() {
+    var name = document.getElementById("nameInput").value;
+    document.getElementById("nameField").innerText = name;
+}
+
+async function downloadPDF() {
+    const { jsPDF } = window.jspdf;
+
+    let certificate = document.getElementById("certificate-container");
+
+    html2canvas(certificate).then(canvas => {
+        let imgData = canvas.toDataURL("image/png");
+        let pdf = new jsPDF('landscape', 'px', [canvas.width, canvas.height]);
+        pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+        pdf.save("Viksit_Bharat_Certificate.pdf");
+    });
+}
